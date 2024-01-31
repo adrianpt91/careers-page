43ed37da464bfc45a71aef1d8dc3ccb6d01e83a5
Adrian Pelaez <adrianpt1991@gmail.com>
2024-01-30 20:12:14 -0500

    Mockup carrers page

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    fontFamily: {
+      'body': ['"Open Sans"'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
