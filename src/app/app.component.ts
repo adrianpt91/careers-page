63bec32ddd16a6a2e98575caa7dd4ac85dadb119
Adrian Pelaez <adrianpt1991@gmail.com>
2024-01-31 19:01:35 -0500

    Fix some issues on mobile screen and adding the modal popup form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,57 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {
+  AUTO_STYLE,
+  animate,
+  state,
+  style,
+  transition,
+  trigger
+} from '@angular/animations';
+
+const DEFAULT_DURATION = 300;
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  animations: [
+    trigger('collapse', [
+      state('false', style({ height: AUTO_STYLE, visibility: AUTO_STYLE })),
+      state('true', style({ height: '0', visibility: 'hidden' })),
+      transition('false => true', animate(DEFAULT_DURATION + 'ms ease-in')),
+      transition('true => false', animate(DEFAULT_DURATION + 'ms ease-out'))
+    ]),
+    trigger('collapse2', [
+      state('false', style({ height: AUTO_STYLE, visibility: AUTO_STYLE })),
+      state('true', style({ height: '0', visibility: 'hidden' })),
+      transition('false => true', animate(DEFAULT_DURATION + 'ms ease-in')),
+      transition('true => false', animate(DEFAULT_DURATION + 'ms ease-out'))
+    ]),
+    trigger('collapse3', [
+      state('false', style({ height: AUTO_STYLE, visibility: AUTO_STYLE })),
+      state('true', style({ height: '0', visibility: 'hidden' })),
+      transition('false => true', animate(DEFAULT_DURATION + 'ms ease-in')),
+      transition('true => false', animate(DEFAULT_DURATION + 'ms ease-out'))
+    ])
+  ]
 })
 export class AppComponent {
   title = 'careers-page';
+  collapsed = true;
+  collapsed2 = true;
+  collapsed3 = true;
+
+  toggle() {
+    this.collapsed = !this.collapsed;
+  }
+
+  toggle2() {
+    this.collapsed2 = !this.collapsed2;
+  }
+  toggle3() {
+    this.collapsed3 = !this.collapsed3;
+  }
 }
